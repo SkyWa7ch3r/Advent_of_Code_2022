6e1d748b3b87066cb0a4d3a43cbdc1d8b78f17e4
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2022-12-06 20:19:22 +0800

    Solved Day_5

--- Day_5/day_5.cpp
+++ Day_5/day_5.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <regex>
 
 struct Move {
     int numberCrates{1};
@@ -12,33 +13,63 @@ struct Move {
 
 
 // Create a vector of stack data structures depending on the number of rows in the top 
-std::vector<std::stack<char>> createCrateStacks(std::ifstream& file) {
-    std::vector<std::stack<char>> crates;
+// Using vector instead of stack so we put in the elements in the correct order
+// We should be able to pop_back and push_back for popping and pushing during instructions
+std::vector<std::vector<char>> createCrateStacks(std::ifstream& file) {
     std::string newLine;
     std::getline(file, newLine);
+    std::vector<std::vector<char>> crates;
+    // Resize the crates by the number of stacks we need
+    crates.resize(newLine.length() / 4 + 1);
     while(newLine.length() != 0) {
-        std::cout << newLine + " " << int(newLine.length()) << "\n";
+        // The only easy way I can think to extract the stupid input is takling advantage of the fact the characters are always 4 characters apart
+        // This can also probably be done with regex match
+        for(int index = 1; index < newLine.length(); index = index + 4) {
+            // If it's a capital character (>= 65 in ASCII)
+            if (int(newLine[index]) >= 65) {
+                // We grab the index / 4, and because insert needs an iterator we tell it where the beginning is
+                crates[index / 4].insert(crates[index / 4].begin(), newLine[index]);
+            }
+        }
         std::getline(file, newLine);
-    }
-    std::cout << "Done with top section Now the instructions\n"; 
-    while (!file.eof()) {
-        std::getline(file, newLine);
-        std::cout << newLine + " " << int(newLine.length()) << "\n";
     }
 
     return crates;
 }
 
-
 // Then follow the proceduces -1 and move items using pop and push to stacks.
 // At the end pop all the stacks and get the final crates on top as a string
-std::string getTopCrates(std::vector<std::stack<char>> crates) {
-
-    return "Crates be here rrrr";
+void craneTheCrates(std::vector<std::vector<char>>& crates, std::ifstream& file, bool craneMaster9001 = false) {
+    std::string instruction;
+    while(!file.eof()) {
+        std::getline(file, instruction);
+        Move currentMove{};
+        // Extract the moves from instructions
+        std::string to = instruction.substr(instruction.rfind(' '), instruction.rfind(' '));
+        std::string from = instruction.substr(instruction.rfind("from"), instruction.rfind("from"));
+        std::string move = instruction.substr(instruction.find("move"), instruction.find("from"));
+        currentMove.numberCrates = stoi(move.substr(move.find(' ') + 1)); 
+        currentMove.stackFrom = stoi(from.substr(from.find(' ') + 1)) - 1;
+        currentMove.stackTo = stoi(to.substr(to.find(' ') + 1)) - 1;
+        // Move every required crate based on instructions
+        if (craneMaster9001) {
+            int oldFromLength = crates[currentMove.stackFrom].size();
+            for (int crate = oldFromLength - currentMove.numberCrates; crate < oldFromLength; crate++) {
+                crates[currentMove.stackTo].push_back(crates[currentMove.stackFrom][crate]);
+            }
+            crates[currentMove.stackFrom].resize(oldFromLength - currentMove.numberCrates);
+        } else {
+            for (int crate = 0; crate < currentMove.numberCrates; crate++) {
+                char popped = crates[currentMove.stackFrom][crates[currentMove.stackFrom].size() - 1];
+                crates[currentMove.stackTo].push_back(popped);
+                crates[currentMove.stackFrom].pop_back();
+            }
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 1) {
+    if (argc < 2) {
         std::cerr << "No Input\n";
         return 1;
     }
@@ -46,8 +77,17 @@ int main(int argc, char* argv[]) {
     if (!input) {
         std::cerr << "File Couldn't be opened\n";
     }
-
-    createCrateStacks(input);
+    std::vector<std::vector<char>> crates = createCrateStacks(input);
+    if (argc > 2) {
+        craneTheCrates(crates, input, true);
+    } else {
+        craneTheCrates(crates, input);
+    }
+    std::cout << "Result: ";
+    for (auto crateStack : crates) {
+        std::cout << crateStack.back();
+    }
+    std::cout << "\n";
 
     return 0;
 }
